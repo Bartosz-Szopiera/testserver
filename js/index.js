d18d360c991dadc4b43b47d0f4641641951f4a88
Bartosz <bartosz.szopiera@gmail.com>
2017-07-21 09:12:53 +0200

    Change header from flexbox to regular block and position text within. Also remove flexboxFix() since it's unnecessary and wasn't good anyway.

--- js/index.js
+++ js/index.js
@@ -65,17 +65,6 @@ function setLanguage(language) {
 setLanguage('pl');
 
 // =============================
-// Add proper justify-content on
-// mobile chrome
-
-(function flexboxFix() {
-  if (window.innerWidth < 900) {
-    var header = document.querySelector('#header header');
-    header.classList.add('fixedHeader');
-  }
-})();
-
-// =============================
 // When changing to the no-sidebar mode function
 // is reading calculated widths of <li> elements
 // and sets them as fixed.
